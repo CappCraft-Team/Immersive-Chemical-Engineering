cf4161461bc549d59c79ebd4b7106af9f92f99d9
SNIPER722 <sniper722@icloud.com>
2021-01-30 04:56:51 -0500

    convert to Java (#1)
    * change to Java
    Co-authored-by: Windmill-City <1449182174@qq.com>

---
+++ src/main/java/team/cappcraft/icheme/ModBlocks.java
@@ -0,0 +1,10 @@
+package team.cappcraft.icheme;
+
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class ModBlocks {
+    @SideOnly(Side.CLIENT)
+    public static void initModels(){
+    }
+}
