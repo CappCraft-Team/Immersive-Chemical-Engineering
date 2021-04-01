4c97f90d0e2052456128c385645640da601ff490
Windmill-City <1449182174@qq.com>
2021-04-01 12:25:46 +0800

    Fix shadow & auto change version

--- src/main/java/team/cappcraft/immersivechemical/ImmersiveChemicalEngineering.java
+++ src/main/java/team/cappcraft/immersivechemical/ImmersiveChemicalEngineering.java
@@ -20,7 +20,7 @@ public class ImmersiveChemicalEngineering {
     //insert an 'immersive' to get around IE's registry
     public static final String MODID = "immersivechemical";
     public static final String NAME = "Immersive Chemical Engineering";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "${version}";
 
     public static Logger logger;
 
